4f786d93f02c6e83dbb3778d7967abaaded0e252
Aslaii <aslaii@aslaii.com>
2023-08-12 16:28:45 +0800

    Exercise 3.22 done

--- backend/index.js
+++ backend/index.js
@@ -30,25 +30,25 @@ const Person = require("./models/person");
 //
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
+        console.log("Method:", request.method);
+        console.log("Path:  ", request.path);
+        console.log("Body:  ", request.body);
+        console.log("---");
+        next();
 };
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+        console.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
+        if (error.name === "CastError") {
+                return response.status(400).send({ error: "malformatted id" });
+        }
 
-  next(error);
+        next(error);
 };
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+        response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(express.json());
@@ -58,95 +58,95 @@ app.use(express.static("dist"));
 app.use(requestLogger);
 
 app.get("/api/persons", (req, res) => {
-  Person.find({}).then((person) => {
-    res.json(person);
-  });
+        Person.find({}).then((person) => {
+                res.json(person);
+        });
 });
 
 app.get("/info", (req, res) => {
-  Person.countDocuments({})
-    .then((count) => {
-      let date = new Date();
-      res.send(`Phonebook has info for ${count} people <br/> ${date}`);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({ error: "An error occurred while getting info." });
-    });
+        Person.countDocuments({})
+                .then((count) => {
+                        let date = new Date();
+                        res.send(`Phonebook has info for ${count} people <br/> ${date}`);
+                })
+                .catch((error) => {
+                        console.error(error);
+                        res.status(500).send({ error: "An error occurred while getting info." });
+                });
 });
 
 app.get("/api/persons/:id", (req, res, next) => {
-  Person.findById(req.params.id)
-    .then((person) => {
-      if (person) res.json(person);
-      else res.json(404).end();
-    })
-    .catch((error) => next(error));
+        Person.findById(req.params.id)
+                .then((person) => {
+                        if (person) res.json(person);
+                        else res.json(404).end();
+                })
+                .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
+        Person.findByIdAndRemove(req.params.id)
+                .then((result) => {
+                        res.status(204).end();
+                })
+                .catch((error) => next(error));
 });
 
 app.post("/api/persons", function(req, res) {
-  const body = req.body;
+        const body = req.body;
 
-  if (!body.name) {
-    return res.status(400).json({
-      error: "name not found",
-    });
-  }
+        if (!body.name) {
+                return res.status(400).json({
+                        error: "name not found",
+                });
+        }
 
-  if (!body.number) {
-    return res.status(400).json({
-      error: "number not found",
-    });
-  }
+        if (!body.number) {
+                return res.status(400).json({
+                        error: "number not found",
+                });
+        }
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
+        const person = new Person({
+                name: body.name,
+                number: body.number,
+        });
 
-  console.log(`Adding ${body.name} ${body.number} to phonebook...`);
-  person
-    .save()
-    .then((result) => {
-      console.log(
-        `added Name: ${body.name} Number: ${body.number} to phonebook. `,
-      );
-      res.json(result);
-    })
-    .catch((error) => {
-      console.log(`Error: ${error}`);
-      res.status(500).json({
-        error: "Something went wrong while adding data.",
-      });
-    });
+        console.log(`Adding ${body.name} ${body.number} to phonebook...`);
+        person
+                .save()
+                .then((result) => {
+                        console.log(
+                                `added Name: ${body.name} Number: ${body.number} to phonebook. `,
+                        );
+                        res.json(result);
+                })
+                .catch((error) => {
+                        console.log(`Error: ${error}`);
+                        res.status(500).json({
+                                error: "Something went wrong while adding data.",
+                        });
+                });
 });
 
 app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body;
+        const body = req.body;
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  };
+        const person = {
+                name: body.name,
+                number: body.number,
+        };
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then((updatedPerson) => {
-      res.json(updatedPerson);
-    })
-    .catch((error) => next(error));
+        Person.findByIdAndUpdate(req.params.id, person, { new: true })
+                .then((updatedPerson) => {
+                        res.json(updatedPerson);
+                })
+                .catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${PORT}`);
 });
--- backend/models/person.js
+++ backend/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.set("strictQuery", false);
 
 const url = process.env.MONGODB_URI;
@@ -6,25 +7,25 @@ const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB", error.message);
-  });
+        .connect(url)
+        .then(() => {
+                console.log("connected to MongoDB");
+        })
+        .catch((error) => {
+                console.log("error connecting to MongoDB", error.message);
+        });
 
 const PhoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+        name: String,
+        number: String,
 });
 
 PhoneBookSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
+        transform: (document, returnedObject) => {
+                returnedObject.id = returnedObject._id.toString();
+                delete returnedObject._id;
+                delete returnedObject.__v;
+        },
 });
 
 module.exports = mongoose.model("Phonebook", PhoneBookSchema);
